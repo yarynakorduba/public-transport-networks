0adc5d9da4d81c126ee888fa3020bfca82c06730
unknown <ko.oles999@gmail.com>
2019-02-11 22:40:09 +0200

    added small changes to TransportGraph

--- src/components/GraphContainer/GraphContainer.js
+++ src/components/GraphContainer/GraphContainer.js
@@ -2,9 +2,12 @@ import React from "react"
 import "./GraphContainer.scss"
 import TransportGraph from "../TransportGraph"
 
-const GraphContainer = () => <TransportGraph nodes={[
-  { x: 50, y: 20 },
-  { x: 200, y: 300 },
-  { x: 300, y: 40 }
-]} width={"300px"} height={"300px"}/>
+const GraphContainer = () =>
+  <div className={"GraphContainer"}>
+    <TransportGraph nodes={[
+      { x: 50, y: 20 },
+      { x: 200, y: 300 },
+      { x: 300, y: 40 }
+    ]} width={"300px"} height={"300px"}/>
+  </div>
 export default GraphContainer
