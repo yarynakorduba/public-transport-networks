5eb7892463b98b11e1ff73b97ae369679cdd5610
unknown <ko.oles999@gmail.com>
2019-02-11 22:40:09 +0200

    added small changes to TransportGraph

--- src/components/TransportGraph/TransportGraph.js
+++ src/components/TransportGraph/TransportGraph.js
@@ -4,7 +4,8 @@ import { Graph } from '@vx/network';
 class TransportGraph extends Component {
   constructor(props) {
     super(props);
-    const {nodes, width, height} = props;
+    const {nodes, width, height, trigger} = props;
+    this.trigger = trigger
 
     const links = [
       { source: nodes[0], target: nodes[1] },
@@ -17,11 +18,33 @@ class TransportGraph extends Component {
       links: links,
       width: width,
       height: height,
+      fill: "#fbce0f"
     };
   }
 
+  isAtPosition() {
+    let spot = document.querySelector(".third").getBoundingClientRect();
+    return (
+      spot.top < 100
+    )
+  }
+
+  componentDidMount(){
+    window.addEventListener('scroll', () => {
+      this.showPicture()
+    });
+  }
+
+  showPicture() {
+    if (this.isAtPosition()) {
+      this.setState({fill: "#272b4d"})
+    } else {
+      this.setState({fill: "#fbce0f"})
+    }
+  }
+
   render() {
-    const {nodes, links, width, height} = this.state
+    const {nodes, links, width, height, fill} = this.state
 
     this.graph = {
       nodes,
@@ -30,7 +53,7 @@ class TransportGraph extends Component {
 
     return(
       <svg width={width} height={height}>
-        <rect width={width} height={height} rx={14} fill="#272b4d" />
+        <rect width={width} height={height} rx={14} fill={fill} />
         <Graph graph={this.graph} />
       </svg>
     )
