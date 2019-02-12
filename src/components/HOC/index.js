4cfa469b339ef064312ba6233f3285ebade80643
yarynakorduba <korduba_y@ucu.edu.ua>
2019-02-13 00:25:44 +0200

    made small refactoring

---
+++ src/components/HOC/index.js
@@ -0,0 +1,84 @@
+import { branch, renderComponent, withProps, compose, withState } from "recompose"
+import { json, pairs, scan } from "d3"
+
+const lengthComparator = (a, b) => a.length - b.length
+
+export const withData = compose(
+  withState("data", "setData"),
+  withProps(async ({ data, setData }) => {
+    if (!data) {
+      const data = await json("bristol_BUS.json", data => data)
+      setData(data)
+    }
+  }),
+  branch(({ data }) => !data, renderComponent(() => "Loading data for the visualization..."))
+)
+
+export const withSmallestRoute = compose(
+  withProps(({ data }) => ({
+    smallestRoute: Object.keys(data)[scan(Object.keys(data), (a, b) => lengthComparator(data[a], data[b]))]
+  })),
+  withProps(({ smallestRoute, data }) => ({
+    smallestRouteStops: data[smallestRoute]
+  }))
+)
+
+export const withBiggestRoute = compose(
+  withProps(({ data }) => ({
+    biggestRoute: Object.keys(data)[scan(Object.keys(data), (a, b) => lengthComparator(data[b], data[a]))]
+  })),
+  withProps(({ biggestRoute, data }) => ({
+    biggestRouteStops: data[biggestRoute]
+  }))
+)
+
+const pair = (a, b) => ({ source: a, target: b })
+
+export const withVisualizationConfig = withProps(({ smallestRouteStops, biggestRouteStops, height, width }) => ({
+  nodeProps: {
+    "1": {
+      backgroundColor: "lemongrass",
+      graph: {
+        nodes: [
+          ...smallestRouteStops.map((stop, index) => ({
+            x: (width / smallestRouteStops.length - 20) * index + 150,
+            y: height / 2.5 + (index % 2) * 100
+          }))
+        ],
+        links: [
+          ...pairs(
+            [
+              ...smallestRouteStops.map((stop, index) => ({
+                x: (width / smallestRouteStops.length - 20) * index + 150,
+                y: height / 2.5 + (index % 2) * 100
+              }))
+            ],
+            pair
+          )
+        ]
+      }
+    },
+    "2": {
+      backgroundColor: "yellow",
+      graph: {
+        nodes: [
+          ...biggestRouteStops.map((stop, index) => ({
+            x: (width / 1.25 / biggestRouteStops.length) * index + 50,
+            y: height / 4 + (index % 5) * 100
+          }))
+        ],
+        links: [
+          ...pairs(
+            [
+              ...biggestRouteStops.map((stop, index) => ({
+                x: (width / 1.25 / biggestRouteStops.length) * index + 50,
+                y: height / 4 + (index % 5) * 100
+              }))
+            ],
+            pair
+          )
+        ]
+      }
+    }
+  }
+}))
