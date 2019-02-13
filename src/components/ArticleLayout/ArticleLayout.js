946b80a00da688857f725ed9da2b1169af1970bb
Mykhailo Ivankiv <neformal.lviv@gmail.com>
2019-02-13 15:28:40 +0200

    Try to find good solution for Page Layout Scrolling.

--- src/components/ArticleLayout/ArticleLayout.js
+++ src/components/ArticleLayout/ArticleLayout.js
@@ -1,27 +1,97 @@
 import React, { Component, createContext } from "react"
+import { dissoc, omit, without } from "ramda"
 
-const TriggerContext = createContext(100)
+const ScrolledContext = createContext()
 
 class ArticleLayout extends Component {
-  constructor(props) {
-    super(props)
+  state = {
+    scrolled: 0,
+    showState: []
   }
-  static defaultProps = { triggerOffset: 100 }
+
+  componentDidMount() {
+    window.addEventListener("scroll", () => {
+      this.setState({
+        scrolled: this.refs.root.getBoundingClientRect().top
+      })
+    })
+  }
+
   render() {
-    const { article, illustration, triggerOffset } = this.props
+    const { article, illustration } = this.props
+    const { scrolled, showState } = this.state
+
     return (
-      <TriggerContext.Provider value={triggerOffset}>
-        <div>{article}</div>
-        <div>{illustration}</div>
-      </TriggerContext.Provider>
+      <section ref="root">
+        <ScrolledContext.Provider
+          value={{
+            scrolled: -scrolled,
+            onAction: data =>
+              this.setState({
+                showState: {
+                  ...showState,
+                  ...data
+                }
+              }),
+            offAction: data =>
+              this.setState({
+                showState: omit(Object.keys(data), showState)
+              })
+          }}
+        >
+          <div
+            style={{
+              color: "red",
+              position: "fixed",
+              top: 10,
+
+              background: "white"
+            }}
+          >
+            {JSON.stringify(showState)}
+          </div>
+
+          <div>{article}</div>
+          <div>{illustration}</div>
+        </ScrolledContext.Provider>
+      </section>
     )
   }
 }
 
-export const Trigger = ({ children }) => (
-  <TriggerContext.Consumer>
-    {value => console.log("===> ", value) || children}
-  </TriggerContext.Consumer>
-)
+export class Trigger extends Component {
+  state = {
+    position: null,
+    triggered: false
+  }
+
+  componentDidMount() {
+    this.setState({ position: this.refs.root.getBoundingClientRect().top })
+  }
+
+  render() {
+    let { children } = this.props
+    let { position, triggered } = this.state
+
+    return (
+      <span ref="root">
+        <ScrolledContext.Consumer>
+          {({ scrolled, onAction, offAction }) => {
+            if (position - scrolled < 0 && !triggered) {
+              this.setState({ triggered: true })
+              onAction(this.props.data)
+            }
+
+            if (position - scrolled > 0 && triggered) {
+              this.setState({ triggered: false })
+              offAction(this.props.data)
+            }
+            return children
+          }}
+        </ScrolledContext.Consumer>
+      </span>
+    )
+  }
+}
 
 export default ArticleLayout
