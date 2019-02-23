98e81a66e1a8a3445a3683a827862be9b35c2158
yarynakorduba <korduba_y@ucu.edu.ua>
2019-02-23 12:25:16 +0200

    added tabs

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,13 @@ import React, { Component } from "react"
 import "../styles/App.scss"
 import ArticleLayout from "./ArticleLayout"
 import Article from "./Article"
-import TransportGraph from "./TransportGraph"
+import LPCGraph from "./LPCGraph"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ArticleLayout article={<Article />} illustration={<TransportGraph />} />
+        <ArticleLayout article={<Article />} illustration={<LPCGraph />} />
       </div>
     )
   }
