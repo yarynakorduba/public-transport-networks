d7069220c4de5c5517ec5c6a71afc985ed0a8457
yarynakorduba <korduba_y@ucu.edu.ua>
2019-03-05 01:49:19 +0200

    added generated svgs for spaces examples

--- src/helpers/scales.js
+++ src/helpers/scales.js
@@ -2,10 +2,11 @@ import * as d3 from "d3"
 
 export const colorScale = d3
   .scaleThreshold()
-  .domain([2, 5])
+  .domain([2, 3])
   .range(["white", "#F3CC06", "#B83C3D"])
+
 
 export const radiusScale = d3
   .scaleLinear()
-  .domain([1, 10])
-  .range([2, 11])
+  .domain([1, 7])
+  .range([5, 15])
