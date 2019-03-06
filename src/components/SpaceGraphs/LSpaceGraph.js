d88cf9d5d3561ec0b9c55ebed8e4c9b3b36361ca
yarynakorduba <korduba_y@ucu.edu.ua>
2019-03-06 18:54:23 +0200

    refactored reducer

--- src/components/SpaceGraphs/LSpaceGraph.js
+++ src/components/SpaceGraphs/LSpaceGraph.js
@@ -2,17 +2,20 @@ import React from "react"
 import { withParentSize } from "@vx/responsive"
 import { branch, compose, defaultProps, lifecycle, renameProps, renderComponent, withProps } from "recompose"
 import * as d3 from "d3"
-import { flatten, indexOf } from "ramda"
+import { flatten, indexOf, values, addIndex, map } from "ramda"
 import { connect } from "react-redux"
 import { fetchStops } from "../../actions"
 import { removeDegreeTwoNode } from "../../actions/actionCreators"
 import { radiusGraphScale } from "../../helpers/scales"
 import withDrawedChart from "../HOC/drawChart"
 import withDragging from "../HOC/dragging"
+import { max, min } from "../../helpers"
 
 const LSpaceGraph = ({ chartHeight, chartWidth }) => (
   <svg className={"LSpaceGraph"} height={chartHeight} width={chartWidth} />
 )
+
+const mapIndexed = addIndex(map)
 
 export default compose(
   connect(
@@ -24,7 +27,8 @@ export default compose(
   defaultProps({
     width: 300,
     height: 600,
-    margin: { top: 0, left: 0, bottom: 0, right: 0 }
+    margin: { top: 0, left: 0, bottom: 0, right: 0 },
+    colorConfig: { domain: [1, 4] }
   }),
   withParentSize,
   renameProps({ parentHeight: "height", parentWidth: "width" }),
@@ -33,6 +37,7 @@ export default compose(
   withProps(({ data, removeDegreeTwoNode }) =>
     Object.keys(data).forEach(async node => data[node].connections.length === 2 && removeDegreeTwoNode(node))
   ),
+
   withProps(
     ({ data, dataToDisplay }) =>
       !dataToDisplay && {
@@ -63,7 +68,10 @@ export default compose(
           .id(d => d.index)
           .strength(0.8)
       )
-      .force("collide", d3.forceCollide(d => (d.connections.length === 1 ? 11 : radiusGraphScale([1, 10])(d.r) + 2)).strength(0.5))
+      .force(
+        "collide",
+        d3.forceCollide(d => (d.connections.length === 1 ? 11 : radiusGraphScale([1, 10])(d.r) + 2)).strength(0.5)
+      )
       .force("charge", d3.forceManyBody())
       .force("center", d3.forceCenter(chartWidth / 2, chartHeight / 2))
       .force("y", d3.forceY(0).strength(0.5))
@@ -72,6 +80,10 @@ export default compose(
   withDragging,
   withProps(() => ({
     DOMElement: ".LSpaceGraph"
+  })),
+  withProps(({ dataToDisplay: { nodes } }) => ({
+    minDegree: nodes[d3.scan(nodes, min)].connections.length,
+    maxDegree: nodes[d3.scan(nodes, max)].connections.length
   })),
   withDrawedChart,
   lifecycle({
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,8 +1,13 @@
-export const arrayToObject = array =>
-  array.reduce((obj, item) => {
-    obj[item.id] = item
+import { reduce, curry, addIndex, map } from "ramda"
+
+export const mapIndexed = addIndex(map)
+
+export const arrayToObject = curry((keyFn, array) =>
+  reduce((obj, item) => {
+    obj[keyFn(item)] = item //this is mutable due to performance
     return obj
-  }, {})
+  }, {})(array)
+)
 
 export const min = (a, b) => a.connections.length - b.connections.length
 export const max = (a, b) => b.connections.length - a.connections.length
