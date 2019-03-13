42c21b75c293f03f03c88dff160868706bda521b
yarynakorduba <korduba_y@ucu.edu.ua>
2019-03-13 10:35:04 +0200

    Added some details to storybook

--- src/components/Article/Article.stories.js
+++ src/components/Article/Article.stories.js
@@ -3,11 +3,20 @@ import { storiesOf } from "@storybook/react"
 import Article from "./Article"
 import { Provider } from "react-redux"
 import configureStore from "../../configureStore"
+import { ScrolledContext } from "../ArticleLayout/ArticleLayout"
 
 const store = configureStore()
-
-storiesOf("Article", module).add("Simple usage", () => (
-  <Provider store={store}>
-    <Article />
-  </Provider>
-))
+const scrolled = 100
+storiesOf("Article", module).add("Simple usage", () => {
+  return (
+    <Provider store={store}>
+      <ScrolledContext.Provider
+        value={{
+          scrolled
+        }}
+      >
+        <Article />
+      </ScrolledContext.Provider>
+    </Provider>
+  )
+})
