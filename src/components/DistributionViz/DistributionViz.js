beab9de03225cfc3227824aa1e4a6acd095accbe
mykhailoivankiv <neformal.lviv@gmail.com>
2019-03-17 22:10:01 +0200

    Clean code. Remove unused files.

--- src/components/DistributionViz/DistributionViz.js
+++ src/components/DistributionViz/DistributionViz.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react"
 import { withParentSize } from "@vx/responsive"
 import { extent, select, scaleLinear, svg, max, json, axisBottom, axisLeft } from "d3"
-import { line } from 'd3-shape'
+import { line } from "d3-shape"
 import { compose, defaultProps, withProps } from "recompose"
 
 import "./DistributionViz.scss"
@@ -18,60 +18,81 @@ export default compose(
   defaultProps({
     width: 500,
     height: 250,
-    margin: { top: 150, left: 100, bottom: 30, right: 50 },
+    margin: { top: 150, left: 100, bottom: 30, right: 50 }
   }),
 
   // visualization preparation
   withProps(({ width, height, margin }) => ({
     drawChart: rootEl => {
-
       const graph = select(rootEl)
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
-        .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-      let x = scaleLinear().range([0, width]);
-      let y = scaleLinear().range([height, 0]);
+      let x = scaleLinear().range([0, width])
+      let y = scaleLinear().range([height, 0])
 
       // Define the axes
       let xAxis = axisBottom().scale(x)
 
-      let yAxis = axisLeft().scale(y).ticks(5)
-        .tickSize(-width);
+      let yAxis = axisLeft()
+        .scale(y)
+        .ticks(5)
+        .tickSize(-width)
 
       // Define the line
       let valueline = line()
-        .x(d => { return x(d.paths); })
-        .y(d => { return y(d.quantity); });
+        .x(d => {
+          return x(d.paths)
+        })
+        .y(d => {
+          return y(d.quantity)
+        })
 
       json("/data/testik.json").then(data => {
-        x.domain(extent(data, d => { return d.paths; }));
-        y.domain([0, max(data, d => { return d.quantity; })]);
+        x.domain(
+          extent(data, d => {
+            return d.paths
+          })
+        )
+        y.domain([
+          0,
+          max(data, d => {
+            return d.quantity
+          })
+        ])
 
-        graph.append("path")
+        graph
+          .append("path")
           .attr("class", b("bristol_c_space"))
-          .attr("d", valueline(data));
+          .attr("d", valueline(data))
 
-        graph.selectAll("dot")
+        graph
+          .selectAll("dot")
           .data(data)
-          .enter().append("circle")
+          .enter()
+          .append("circle")
           .attr("r", 3)
-          .attr("cx", d => { return x(d.paths); })
-          .attr("cy", d => { return y(d.quantity); })
+          .attr("cx", d => {
+            return x(d.paths)
+          })
+          .attr("cy", d => {
+            return y(d.quantity)
+          })
           .attr("fill", "#1B1733")
 
-        graph.append("g")
+        graph
+          .append("g")
           .attr("class", b("axis"))
           .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
+          .call(xAxis)
 
-        graph.append("g")
+        graph
+          .append("g")
           .attr("class", b("axis"))
-          .call(yAxis);
-
-      });
+          .call(yAxis)
+      })
     }
   }))
 )(DistributionViz)
