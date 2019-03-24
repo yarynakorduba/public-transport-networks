17b8fec418aad1ea288b0a71b8a19032e0ea2d36
mykhailoivankiv <neformal.lviv@gmail.com>
2019-03-24 22:26:02 +0200

    Add Bristol heatmap.

--- src/components/HeatMap/HeatMap.js
+++ src/components/HeatMap/HeatMap.js
@@ -10,7 +10,7 @@ const convertBusStopsDataToGeoJSON = (data) => ({
   "type": "FeatureCollection",
   "features": data.map( ({lat,lon, routes}) => ({
     "type": "Feature",
-    "properties": { "connectedRoutes": routes.length, "mag": 2.3 },
+    "properties": { "connectedRoutes": routes.length,},
     "geometry": { "type": "Point", "coordinates": [lon, lat] } }))
 })
 
@@ -25,7 +25,7 @@ const HeatMap = () => {
 
   const mapRef = useRef()
 
-  const MAX_ZOOM_LEVEL = 13
+  const MAX_ZOOM_LEVEL = 15
 
   const getMap = () => (mapRef.current ? mapRef.current.getMap() : null)
 
@@ -37,10 +37,22 @@ const HeatMap = () => {
     type: "heatmap",
     paint: {
       // Increase the heatmap weight based on frequency and property magnitude
-      "heatmap-weight": ["interpolate", ["linear"], ["get", "mag"], 0, 0, 6, 1],
+      "heatmap-weight": [
+        "interpolate",
+        ["linear"],
+        ["get", "connectedRoutes"],
+        0, 0,
+        MAX_ZOOM_LEVEL, 1
+      ],
       // Increase the heatmap color weight weight by zoom level
       // heatmap-intensity is a multiplier on top of heatmap-weight
-      "heatmap-intensity": ["interpolate", ["linear"], ["zoom"], 0, 1, MAX_ZOOM_LEVEL, 3],
+      "heatmap-intensity": [
+        "interpolate",
+        ["linear"],
+        ["zoom"],
+        0, 1,
+        1, 15
+      ],
       // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
       // Begin color ramp at 0-stop with a 0-transparancy color
       // to create a blur-like effect.
@@ -48,23 +60,29 @@ const HeatMap = () => {
         "interpolate",
         ["linear"],
         ["heatmap-density"],
-        0,
-        "rgba(33,102,172,0)",
-        0.2,
-        "rgb(103,169,207)",
-        0.4,
-        "rgb(209,229,240)",
-        0.6,
-        "rgb(253,219,199)",
-        0.8,
-        "rgb(239,138,98)",
-        0.9,
-        "rgb(255,201,101)"
+        0, "rgba(33,102,172,0)",
+        0.2, "rgb(103,169,207)",
+        0.4, "rgb(209,229,240)",
+        0.6, "rgb(253,219,199)",
+        0.8, "rgb(239,138,98)",
+        1, "rgb(178,24,43)"
       ],
       // Adjust the heatmap radius by zoom level
-      "heatmap-radius": ["interpolate", ["linear"], ["zoom"], 0, 2, MAX_ZOOM_LEVEL, 20],
+      "heatmap-radius": [
+        "interpolate",
+        ["linear"],
+        ["zoom"],
+        0, 2,
+        MAX_ZOOM_LEVEL, 20
+      ],
       // Transition from heatmap to circle layer by zoom level
-      "heatmap-opacity": ["interpolate", ["linear"], ["zoom"], 7, 1, 9, 0]
+      "heatmap-opacity": [
+        "interpolate",
+        ["linear"],
+        ["zoom"],
+        5, 1,
+        MAX_ZOOM_LEVEL, 1
+      ],
     }
   })
 
@@ -73,10 +91,6 @@ const HeatMap = () => {
 
     const bristolStops = await (await fetch("/data/bristol_BUS_stops.json")).json()
     const bristolStopsGeoJSON = convertBusStopsDataToGeoJSON(bristolStops)
-    console.log(bristolStopsGeoJSON)
-
-    let response = await fetch("/data/earthquakes.geojson")
-    const earthquakes = await response.json()
 
     map.addSource(HEATMAP_SOURCE_ID, { type: "geojson", data: bristolStopsGeoJSON })
     map.addLayer(mkHeatmapLayer("heatmap-layer", HEATMAP_SOURCE_ID))
