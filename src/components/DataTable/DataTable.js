20654712b8eb3c100f371188cf98a69861a5d716
unknown <ko.oles999@gmail.com>
2019-03-26 22:23:21 +0200

    Small fixes

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -1,40 +1,41 @@
-import React from 'react';
+import React from "react"
+import { compose, withProps } from "recompose"
 
-const DataTable = () => (
-    <table>
-        <tbody>
-        <tr>
-            <th>City</th>
-            <td>Lviv</td>
-            <td>Bristol</td>
-            <td>London</td>
-        </tr>
-        <tr>
-            <th>Routes Quantity</th>
-            <td>260</td>
-            <td>5900</td>
-            <td>23</td>
-        </tr>
-        <tr>
-            <th>Stops Quantity</th>
-            <td>260</td>
-            <td>5900</td>
-            <td>23</td>
-        </tr>
-        <tr>
-            <th>Population</th>
-            <td>260</td>
-            <td>5900</td>
-            <td>23</td>
-        </tr>
-        <tr>
-            <th>Population</th>
-            <td>260</td>
-            <td>5900</td>
-            <td>23</td>
-        </tr>
-        </tbody>
-    </table>
-);
+import "./DataTable.scss"
+import BEM from "../../helpers/BEM"
 
-export default DataTable
+import data from "../../table_testik"
+
+const b = BEM("data-table")
+
+
+const DataTable = ({ drawChart }) => {
+  return drawChart()
+}
+
+export default compose(
+  withProps(({
+      drawChart: () => {
+        return (
+          <table className={b()}>
+            <tbody>
+            <tr>
+              <th className={b("row-title")}>City</th>
+              <td className={b("row-title")}>Lviv</td>
+              <td className={b("row-title")}>Bristol</td>
+              <td className={b("row-title")}>London</td>
+            </tr>
+            {data.map((item,i)=>(
+              <tr key={i}>
+                <th className={b("row-property")}>{item.property}</th>
+                <td className={b("row-value")}>{item.lviv}</td>
+                <td className={b("row-value")}>{item.bristol}</td>
+                <td className={b("row-value")}>{item.london}</td>
+              </tr>
+            ))}
+            </tbody>
+          </table>
+        )
+      }
+    })
+  ))(DataTable)
