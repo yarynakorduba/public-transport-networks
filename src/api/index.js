c7d684bc855b112d1834ab8036f8c3816711d865
yarynakorduba <korduba_y@ucu.edu.ua>
2019-03-30 00:48:24 +0200

    Added testable gh-pages link.

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,6 @@
-export const getGraphSpaceData = (city:string, space:string): Promise<Array> =>
-  fetch(`/data/${city}_${space}_space.json`)
+export const getGraphSpaceData = (city: string, space: string): Promise<Array> =>
+  fetch(
+    `https://media.githubusercontent.com/media/yarynakorduba/public-transport-networks/master/public/data/${city}_${space}_space.json`
+  )
     .then(response => response.json())
     .catch(e => console.error(e))
