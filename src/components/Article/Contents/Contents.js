bae563e3c143c97119e67850b3e986c0a4615021
unknown <ko.oles999@gmail.com>
2019-05-28 17:56:36 +0300

    Refactored Contents

--- src/components/Article/Contents/Contents.js
+++ src/components/Article/Contents/Contents.js
@@ -1,4 +1,3 @@
-import { Link } from "react-scroll"
 import Latex from "react-latex"
 import React from "react"
 import BEM from "../../../helpers/BEM"
@@ -10,182 +9,54 @@ const Contents = () => (
   <div className={b()}>
     <h2 className={b("header")}>Зміст</h2>
     <ul className={b("list")}>
-      <Link
-        className={b("chapter")}
-        activeClass="active"
-        to="chapter1"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
+      <a className={b("chapter")} href="#chapter1">
         Опис проблеми та Історичний прецедент
-      </Link>
-      <Link
-        className={b("chapter")}
-        activeClass="active"
-        to="chapter2"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
+      </a>
+      <a className={b("chapter")} href="#chapter2">
         Історія двох міст
-      </Link>
-      <Link
-        className={b("subchapter")}
-        activeClass="active"
-        to="chapter2"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
+      </a>
+      <a className={b("subchapter")} href="#chapter2">
         Брістоль
-      </Link>
-      <Link
-        className={b("subchapter")}
-        activeClass="active"
-        to="chapter2"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
+      </a>
+      <a className={b("subchapter")} href="#chapter2">
         Львів
-      </Link>
-      <Link
-        className={b("chapter")}
-        activeClass="active"
-        to="chapter3"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
+      </a>
+      <a className={b("chapter")} href="#chapter3">
         Громадський транспорт як складна мережа
-      </Link>
-      <Link
-        className={b("chapter")}
-        activeClass="active"
-        to="chapter4"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
+      </a>
+      <a className={b("chapter")} href="#chapter4">
         Обробка даних
-      </Link>
-      <Link
-        className={b("chapter")}
-        activeClass="active"
-        to="chapter5"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
+      </a>
+      <a className={b("chapter")} href="#chapter5">
         Топологічні характеристики мереж
-      </Link>
-      <Link
-        className={b("subchapter")}
-        activeClass="active"
-        to="chapter5"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
+      </a>
+      <a className={b("subchapter")} href="#chapter5">
         Характеристики в <Latex>$\mathbb L$-space</Latex>
-      </Link>
-      <Link
-        className={b("subchapter")}
-        activeClass="active"
-        to="chapter6"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
+      </a>
+      <a className={b("subchapter")} href="#chapter6">
         Характеристики в <Latex>$\mathbb P$</Latex>-space
-      </Link>
-      <Link
-        className={b("subchapter")}
-        activeClass="active"
-        to="chapter7"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
+      </a>
+      <a className={b("subchapter")} href="#chapter7">
         Характеристики в <Latex>$\mathbb C$</Latex>-space
-      </Link>
-      <Link
-        className={b("chapter")}
-        activeClass="active"
-        to="chapter8"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
+      </a>
+      <a className={b("chapter")} href="#chapter8">
         Стійкість мереж до атак
-      </Link>
-      <Link
-        className={b("subchapter")}
-        activeClass="active"
-        to="chapter8"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
+      </a>
+      <a className={b("subchapter")} href="#chapter8">
         Атаки в <Latex>$\mathbb L$</Latex>-space
-      </Link>
-      <Link
-        className={b("subchapter")}
-        activeClass="active"
-        to="chapter8"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
+      </a>
+      <a className={b("subchapter")} href="#chapter8">
         Атаки в <Latex>$\mathbb P$</Latex>-space
-      </Link>
-      <Link
-        className={b("subchapter")}
-        activeClass="active"
-        to="chapter8"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
+      </a>
+      <a className={b("subchapter")} href="#chapter8">
         Атаки в <Latex>$\mathbb C$</Latex>-space
-      </Link>
-      <Link
-        className={b("chapter")}
-        activeClass="active"
-        to="chapter9"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
+      </a>
+      <a className={b("chapter")} href="#chapter9">
         Топологічні характеристики в P space
-      </Link>
-      <Link
-        className={b("chapter")}
-        activeClass="active"
-        to="chapter10"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
+      </a>
+      <a className={b("chapter")} href="#chapter10">
         Що далі?
-      </Link>
+      </a>
     </ul>
   </div>
 )
