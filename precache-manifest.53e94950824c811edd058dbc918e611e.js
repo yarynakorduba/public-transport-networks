57d60683ef615190b52fe1c37405c2aa567b8a65
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-29 11:38:07 +0000

    Deploy yarynakorduba/public-transport-networks to github.com/yarynakorduba/public-transport-networks.git:gh-pages

---
+++ precache-manifest.53e94950824c811edd058dbc918e611e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3ecab3f1d04670e2376",
+    "url": "/public-transport-networks/static/css/main.9951d5a9.chunk.css"
+  },
+  {
+    "revision": "a3ecab3f1d04670e2376",
+    "url": "/public-transport-networks/static/js/main.28cfbfbe.chunk.js"
+  },
+  {
+    "revision": "7641de751ce2de2dfbe3",
+    "url": "/public-transport-networks/static/js/runtime~main.cb9eb0c3.js"
+  },
+  {
+    "revision": "538dcc05cc98df607130",
+    "url": "/public-transport-networks/static/js/2.db44e98a.chunk.js"
+  },
+  {
+    "revision": "ce04d8072824da1b432b01eb60409bd3",
+    "url": "/public-transport-networks/index.html"
+  }
+];
