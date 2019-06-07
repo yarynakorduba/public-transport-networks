bccf30d1e636c9da3adfdc795ba3290f1a5691da
yarynakorduba <korduba_y@ucu.edu.ua>
2019-06-07 18:38:38 +0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const cors = require("cors")
 const path = require("path")
 
 const app = express()
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 4000
 //allow cross-origin requests
 app.use(cors())
 
