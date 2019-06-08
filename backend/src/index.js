df3f7ddaca500e624860c6045e078f8097379775
Oles <ko.oles999@gmail.com>
2019-06-08 17:44:07 +0300

    Ready for heroku

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "..", "public")))
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "..", "..", "frontend", "public", "index.html"))
 })
-//;
+
 app.listen(PORT, () => {
   console.log(`Listening for requests on port ${PORT}...`)
 })
