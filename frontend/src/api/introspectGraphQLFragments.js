9d21fafdeaa1488868bbaad4f8428f59de754c8a
yarynakorduba <korduba_y@ucu.edu.ua>
2019-06-11 11:09:20 +0300

    Add small fixes to fragments introspection.

--- frontend/src/api/introspectGraphQLFragments.js
+++ frontend/src/api/introspectGraphQLFragments.js
@@ -26,7 +26,7 @@ fetch(`http://localhost:4000/graphql`, {
     // here we're filtering out any type information unrelated to unions or interfaces
     const filteredData = result.data.__schema.types.filter(type => type.possibleTypes !== null)
     result.data.__schema.types = filteredData
-    fs.writeFile("./fragmentTypes.json", JSON.stringify(result.data), err => {
+    fs.writeFile("./src/api/fragmentTypes.json", JSON.stringify(result.data), err => {
       if (err) {
         console.error("Error writing fragmentTypes file", err)
       } else {
